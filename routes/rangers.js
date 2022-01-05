c8c61d92150fe0170a5f73d843878e950f09874e
Kayci Buchanan <92894323+KayKay2000@users.noreply.github.com>
2022-01-04 20:20:26 -0500

    found some errors, but not all
    Signed-off-by: Kayci Buchanan <92894323+KayKay2000@users.noreply.github.com>

--- routes/rangers.js
+++ routes/rangers.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const rangersModel = require('../db');
+const rangersModel = require('../model/db');
 
 router.get('/', (req, res) => {
     res.render('template', {
@@ -17,12 +17,12 @@ router.get('/', (req, res) => {
 
 router.get('/:slug', (req, res) => {
     const { slug } = req.params;
-    const ranger = rangersModel.find((ranger => ranger.slug === slug)
-    if (ranger) {
+    const ranger = rangersModel.find(ranger => {
+    if (ranger.slug === slug) {
         res.render('template', {
             locals: {
                 title: `Power Ranger: ${ranger.name}`,
-                ranger
+                
             },
             partials: {
                 body: 'partials/ranger-details',
@@ -32,4 +32,7 @@ router.get('/:slug', (req, res) => {
         res.status(404).send(`No Ranger found that matches slug, ${slug}`);
     }
 
-});
+})
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -2,24 +2,23 @@
 
 const hostname = '127.0.0.1';
 const port = 3333;
-
-const express = require('express'),
-    es6Renderer = require('express-es6-template-engine');
-
+const rootController = require('./routes/index');
+const rangersController = require('./routes/rangers');
+const express = require('express');
+const es6Renderer = require('express-es6-template-engine');
 const app = express();
+const server = createServer(app);
 
 app.engine('html', es6Renderer);
 app.set('views', './views');
 app.set('view engine', 'html');
 
-const server = http.createServer(app);
+
 
 server.listen(PORT, HOSTNAME, () => {
     console.log(`Server running at http://${hostname}:${port}`)
 });
 
-const rootController = require('./routes/index');
-const rangersController = require('./routes/rangers');
 
 
 app.use('/rangers', rangersController);
